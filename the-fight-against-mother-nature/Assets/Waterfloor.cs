9695d1a0e395d4f8b59b30792bdb6d9d49635192
JornKrols <krolsjorn1@gmail.com>
2023-01-16 09:57:14 +0100

    trying to fix errors and deleted asset that was not used

---
+++ the-fight-against-mother-nature/Assets/Waterfloor.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Waterfloor : MonoBehaviour
+{
+    public Transform player;
+
+    void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag == "Player")
+        {
+            //RespawnAtCheckpoint();
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+    /*private void RespawnAtCheckpoint()
+    {
+        if (PlayerPrefs.HasKey("CheckpointX"))
+        {
+            Vector3 checkpointPos = new Vector3(PlayerPrefs.GetFloat("CheckpointX"), PlayerPrefs.GetFloat("CheckpointY"), PlayerPrefs.GetFloat("CheckpointZ"));
+            player.position = checkpointPos;
+            SceneManager.LoadScene("MiniGame2");
+            
+        }
+        else
+        {
+            SceneManager.LoadScene("MiniGame2");
+        }
+    }*/
+}
